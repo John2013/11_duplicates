cef9396dea6667df93c9ff7f11153b376c7b93a3
John2013 <NaWasington@yandex.ru>
2018-01-14 02:03:21 +0300

    заменил scandir на walk

--- duplicates.py
+++ duplicates.py
@@ -1,49 +1,40 @@
 import sys
-import re
 from collections import defaultdict
-from os.path import isdir, realpath
-from os import scandir
+from os.path import isdir, realpath, getsize
+from os import walk
 
 
 def get_files_recursively(path):
     files_list = []
 
-    for dir_item in scandir(path):
-        if dir_item.is_dir() and not dir_item.name.startswith('.'):
-            files_list += get_files_recursively(dir_item.path)
-        elif dir_item.is_file:
-            files_list.append(dir_item)
+    for dir, ignored, files in walk(path):
+        for file_name in files:
+            path = realpath("{}/{}".format(dir, file_name))
+            files_list.append({
+                "name": file_name,
+                "path": path,
+                "size": getsize(path)
+            })
 
     return files_list
 
 
 def are_duplicates(file1, file2):
-    size_index = 6
-
-    return \
-        file1.name == file2.name and \
-        file1.stat()[size_index] == file2.stat()[size_index]
-
-
-def get_real_path(path):
-    return re.sub(r'\\', '/', realpath(path))
+    return file1["path"] != file2["path"] and file1["name"] == file2[
+        "name"
+    ] and file1["size"] == file2["size"]
 
 
 def get_duplicates(files_list):
-    duplicates = set()
-    structed_duplicates = defaultdict(set)
+    duplicates = defaultdict(set)
 
     for file1 in files_list:
-        if file1 in duplicates:
-            continue
         for file2 in files_list:
-            if file1.path == file2.path or file2 in duplicates:
-                continue
             if are_duplicates(file1, file2):
-                structed_duplicates[file1.name].add(get_real_path(file1.path))
-                structed_duplicates[file1.name].add(get_real_path(file2.path))
+                duplicates[file1["name"]].add(file1["path"])
+                duplicates[file1["name"]].add(file2["path"])
 
-    return structed_duplicates
+    return duplicates
 
 
 def show_duplicates(duplicates):
